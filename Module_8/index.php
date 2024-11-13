0daa39f2a173f53d965b56ba45b4ff8944d36656
Gerta431 <musligerta08@gmail.com>
2024-11-13 17:47:55 +0100

    update

--- Module_8/index.php
+++ Module_8/index.php
@@ -1,2 +1,24 @@
-<?php
+<?ph
+//creating a table using PDO
+
+$host = 'localhost';
+$user = 'root':
+$db = 'db';
+$pass = '';
+
+try{
+    $pdo = new PDO("mysql:host=$host;dbmane=$db", $user, $pass);
+
+    $dql = "CREATE TABLE users(id INT(6) NOT NULL  AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR(20) NOT NULL)";
+
+    $pdo -> exec($sql);
+
+    echo "table created successfully";
+}catch(exeption $e){
+
+    echo "Error while creating the table".$e.getMessage();
+}
+
 ?>
